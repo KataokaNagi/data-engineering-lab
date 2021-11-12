3915f21e081d37916d9fbf56e8ccb1776bc6cfe7
KataokaNagi <calm1836@gmail.com>
2021-11-12 12:27:03 +0900

    fix: fix file name & gawk -> mawk

--- experiment/preprocess_02_awk.sh
+++ experiment/preprocess_02_awk.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #
-# @file      preprocess_02.awk
+# @file      preprocess_02_awk.sh
 # @author    Kataoka Nagi (calm1836[at]gmail.com)
 # @brief     apply sed on each articles
 # @date      2021-11-10 04:50:13
 # @version   1.0
 # @copyright (c) 2021 Kataoka Nagi This src is released under the MIT License, see LICENSE.
 # 
-gawk -f preprocess_02.awk India-Articles_preprocess-01_extract-column-c.csv > India-Articles_preprocess_02_awk.txt
-gawk -f preprocess_02.awk Japan-Articles_preprocess-01_extract-column-c.csv > Japan-Articles_preprocess_02_awk.txt
-gawk -f preprocess_02.awk Korea-Articles_preprocess-01_extract-column-c.csv > Korea-Articles_preprocess_02_awk.txt
-# gawk -f preprocess_02.awk UK-Articles_preprocess-01_extract-column-c.csv > UK-Articles_preprocess_02_awk.txt
+mawk -f preprocess_02.awk India-Articles_preprocess-01_extract-column-c.csv > India-Articles_preprocess_02_awk.txt
+mawk -f preprocess_02.awk Japan-Articles_preprocess-01_extract-column-c.csv > Japan-Articles_preprocess_02_awk.txt
+mawk -f preprocess_02.awk Korea-Articles_preprocess-01_extract-column-c.csv > Korea-Articles_preprocess_02_awk.txt
+# mawk -f preprocess_02.awk UK-Articles_preprocess-01_extract-column-c.csv > UK-Articles_preprocess_02_awk.txt
