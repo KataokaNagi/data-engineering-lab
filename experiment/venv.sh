4a1bbe08a65a6278b71fe5b348a71dd6d43c2616
KataokaNagi <calm359@gmail.com>
2021-11-17 20:01:04 +0900

    add: bash for venv setup (WIP)     failed:     type python3-venv     activate venv automatically     install stanza automatically

---
+++ experiment/venv.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# @file      venv.sh
+# @author    Kataoka Nagi (calm1836[at]gmail.com)
+# @brief     (WIP) make venv if not making & pip3 install
+# @date      2021-11-11 13:17:34
+# @version   1.0
+# @see       [venv: Python 仮想環境管理](https://qiita.com/fiftystorm36/items/b2fd47cf32c7694adc2e#%E6%96%B0%E3%81%97%E3%81%84%E7%92%B0%E5%A2%83%E3%81%AE%E4%BD%9C%E6%88%90)
+# @see       [[bash]ディレクトリが存在しない場合は作成。存在かつ空でない場合は警告を出力して終了する構文](https://qiita.com/s-katsumata/items/f23fb3fb8c2fc75003a1)
+# @copyright (c) 2021 Kataoka Nagi This src is released under the MIT License, see LICENSE.
+# 
+VENV_DIR=./venv
+ACTIVATE_DIR_FROM_VENV_DIR=bin/activate
+ACTIVATE_DIR="$VENV_DIR/$ACTIVATE_DIR_FROM_VENV_DIR"
+
+function install_if_not_exist() {
+    # $1=installer, $2=command
+    if !(type "$2" > /dev/null 2>&1); then
+        "$1" install "$2";
+    else
+        echo "$2" "is already installed.";
+    fi
+}
+
+install_if_not_exist "apt-get" "python3-venv";
+
+# make venv if not exist
+if [ ! -d $VENV_DIR ]; then
+    python3 -m venv venv;
+fi
+
+# activate venv
+source $ACTIVATE_DIR;
+
+# install by pip3 in venv
+install_if_not_exist "pip3" "cython";
+install_if_not_exist "pip3" "spacy";
+install_if_not_exist "pip3" "stanza";
+
+# set path
+# export PYTHONPATH="$PYTHONPATH:/home/nagi/.local/lib/python3.6/site-packages" # tmp (only in current shell)
+# echo 'export PYTHONPATH="$PYTHONPATH:/home/nagi/.local/lib/python3.6/site-packages"' >> ~/.bashrc # semipermanent
+
+# deactivate venv
+# deactivate
