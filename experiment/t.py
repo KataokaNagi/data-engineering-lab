6514138b3ef3517e942eec838bd4e4987d34d6f9
calm1836@gmail.com <calm1836@gmail.com>
2021-11-28 20:28:32 +0900

    update: check if multi argparse can use (cannot)

--- experiment/t.py
+++ experiment/t.py
@@ -1,28 +1,57 @@
-import re
+# import re
 
-DIST_DIRS = [
-    "./covid-19-news-articles/india-articles_preprocess_03_spilt-sentences.txt",
-    "./covid-19-news-articles/japan-articles_preprocess_03_spilt-sentences.txt",
-    "./covid-19-news-articles/korea-articles_preprocess_03_spilt-sentences.txt"
-    # "./covid-19-news-articles/uk-articles_preprocess_03_spilt-sentences.txt"
-]
+# DIST_DIRS = [
+#     "./covid-19-news-articles/india-articles_preprocess_03_spilt-sentences.txt",
+#     "./covid-19-news-articles/japan-articles_preprocess_03_spilt-sentences.txt",
+#     "./covid-19-news-articles/korea-articles_preprocess_03_spilt-sentences.txt"
+#     # "./covid-19-news-articles/uk-articles_preprocess_03_spilt-sentences.txt"
+# ]
 
-do_debug = True
-use_spacy = False
-use_stanza = True
+# do_debug = True
+# use_spacy = False
+# use_stanza = True
 
-# edit DIST_SIRS according to options
-for i, dir in enumerate(DIST_DIRS):
-    if use_spacy:
-        DIST_DIRS[i] = re.sub("\.txt","_with-spacy.txt", dir)
-    if use_stanza:
-        DIST_DIRS[i] = re.sub("\.txt","_with-stanza.txt", dir)
-        print("use stanza")
+# # edit DIST_SIRS according to options
+# for i, dir in enumerate(DIST_DIRS):
+#     if use_spacy:
+#         DIST_DIRS[i] = re.sub("\.txt","_with-spacy.txt", dir)
+#     if use_stanza:
+#         DIST_DIRS[i] = re.sub("\.txt","_with-stanza.txt", dir)
+#         print("use stanza")
 
-    if do_debug:
-        DIST_DIRS[i] = re.sub("\.txt","_debug.txt", dir)
-    # print(dir)
-    print(DIST_DIRS[i])
+#     if do_debug:
+#         DIST_DIRS[i] = re.sub("\.txt","_debug.txt", dir)
+#     # print(dir)
+#     print(DIST_DIRS[i])
 
-for dir in DIST_DIRS:
-    print(dir)
+# for dir in DIST_DIRS:
+#     print(dir)
+
+##################################################
+
+from argparse import ArgumentParser
+
+ap1 = ArgumentParser(description='one')
+ap2 = ArgumentParser(description='two')
+
+ap1.add_argument(
+    "-o",
+    "--one",
+    help="one description",
+    action="store_true")
+# ap2.add_argument(
+#     "-t",
+#     "--two",
+#     help="two description",
+#     action="store_true")
+
+arg1 = ap1.parse_args()
+# arg2 = ap2.parse_args()
+
+print("foo")
+
+if arg1.one:
+    print("one")
+
+# if arg2.two:
+#     print("two")
