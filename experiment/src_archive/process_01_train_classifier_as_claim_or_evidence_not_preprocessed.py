bc80e40596acc0908e58a8e3215228f12ac0dc85
calm1836@gmail.com <calm1836@gmail.com>
2021-12-05 01:48:23 +0900

    update: apply for preprocess data

--- experiment/src_archive/process_01_train_classifier_as_claim_or_evidence_not_preprocessed.py
+++ experiment/src_archive/process_01_train_classifier_as_claim_or_evidence_not_preprocessed.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
-"""process_01_train_classifier_as_claim_or_evidence.py
+"""process_01_train_classifier_as_claim_or_evidence_not_preprocessed.py
 
 @author    Kataoka Nagi (calm1836[at]gmail.com)
 @brief     train transformer txt classifier as claim or evidence
 @note      [sent-1#sent-2#...\n, ...] -> [e;[ec-score-array];sent-1#c;[ec-score-array];sent-2...\n, ...]
-@note      python3 process_01_train_classifier_as_claim_or_evidence.py
-@date      2021-12-04 06:20:06
-@version   1.1
-@history   apply for preprocess data
+@note      python3 process_01_train_classifier_as_claim_or_evidence_not_preprocessed.py
+@date      2021-12-01 00:38:55
+@version   1.0
 @see       transformer_classifier_as_claim_or_evidence.ipynb
 @see       [hands-on-ml 16章 Sentiment Analysys](https://github.com/ageron/handson-ml2/blob/master/16_nlp_with_rnns_and_attention.ipynb)
 @copyright This file includes the work that is distributed in the [Apache License 2.0](http://www.apache.org/licenses/LICENSE-2.0)
@@ -25,8 +24,8 @@ import logging
 import sklearn
 import time
 
-CLAIMS_DIR = "./IBM_Debater_(R)_CE-EMNLP-2015.v3/claims_preprocess-02_awk.txt"
-EVIDENCE_DIR = "./IBM_Debater_(R)_CE-EMNLP-2015.v3/evidence_preprocess-02_awk.txt"
+CLAIMS_DIR = "./IBM_Debater_(R)_CE-EMNLP-2015.v3/claims.txt"
+EVIDENCE_DIR = "./IBM_Debater_(R)_CE-EMNLP-2015.v3/evidence.txt"
 
 CLAIMS_LABEL = 1
 EVIDENCE_LABEL = 0
@@ -41,47 +40,60 @@ CLASSIFICATION_MODEL_NAME = 'roberta-base'
 
 
 def main():
+    CLAIMS_IDX = 2
+    EVIDENCE_IDX = 2
+
+    claims_data = []
+    evidence_data = []
     claims = []
-    evidences = []
+    evidence = []
 
     # load
     log.d("*** load data of claim & evidence***")
 
-    # import claim data
     with open(CLAIMS_DIR, "r", encoding="utf_8") as f:
-        sentences = f.readlines()
+        articles_sentences = f.readlines()
         log.v("claims.txt lines:")
-        log.v(sentences[0])
-        log.v(sentences[1])
-        log.v(sentences[2])
+        log.v(articles_sentences[0])
+        log.v(articles_sentences[1])
+        log.v(articles_sentences[2])
         log.v()
-        for sentence in sentences:
-            claims.append(sentence.rstrip("\n"))
-        if claims[-1] is None or len(claims[-1]) == 0:
-            claims.pop(-1)
+        for line in articles_sentences[1:]:
+            fields = line.rstrip("\n").split("\t")
+            claims_data.append(fields)
+            claims.append(fields[CLAIMS_IDX])
 
-    # import evidence data
     with open(EVIDENCE_DIR, "r", encoding="utf_8") as f:
-        sentences = f.readlines()
+        articles_sentences = f.readlines()
         log.v("evidence.txt lines:")
-        log.v(sentences[0])
-        log.v(sentences[1])
-        log.v(sentences[2])
+        log.v(articles_sentences[0])
+        log.v(articles_sentences[1])
+        log.v(articles_sentences[2])
         log.v()
-        for sentence in sentences:
-            evidences.append(sentence.rstrip("\n"))
-        if evidences[-1] is None or len(evidences[-1]) == 0:
-            evidences.pop(-1)
+        for line in articles_sentences:
+            fields = line.rstrip("\n").split("\t")
+            evidence_data.append(fields)
+            evidence.append(fields[EVIDENCE_IDX].lower())
 
     # print debug
+    log.v("claims_data:")
+    log.v(claims_data[0])
+    log.v(claims_data[1])
+    log.v()
+
+    log.v("evidence_data:")
+    log.v(evidence_data[0])
+    log.v(evidence_data[1])
+    log.v()
+
     log.v("claims:")
     log.v(claims[0])
     log.v(claims[1])
     log.v()
 
     log.v("evidence:")
-    log.v(evidences[0])
-    log.v(evidences[1])
+    log.v(evidence[0])
+    log.v(evidence[1])
 
     """### ラベリング"""
     log.d("*** labeling claim & evidence ***")
@@ -94,9 +106,9 @@ def main():
         claims_labels = np.zeros(len(claims)).tolist()
 
     if (EVIDENCE_LABEL):
-        evidence_labels = np.ones(len(evidences)).tolist()
+        evidence_labels = np.ones(len(evidence)).tolist()
     else:
-        evidence_labels = np.zeros(len(evidences)).tolist()
+        evidence_labels = np.zeros(len(evidence)).tolist()
 
     log.v(claims_labels[0])
     log.v(evidence_labels[0])
@@ -107,8 +119,8 @@ def main():
     for idx in range(len(claims)):
         claims_dataset.append([claims[idx], claims_labels[idx]])
 
-    for idx in range(len(evidences)):
-        evidence_dataset.append([evidences[idx], evidence_labels[idx]])
+    for idx in range(len(evidence)):
+        evidence_dataset.append([evidence[idx], evidence_labels[idx]])
 
     log.v(claims_dataset[0])
     log.v(evidence_dataset[0])
