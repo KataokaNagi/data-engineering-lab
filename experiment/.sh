5b1f8f756127fc44a52619959ec85cb4b0333ad9
calm1836@gmail.com <calm1836@gmail.com>
2021-12-15 22:56:27 +0900

    add: preprocess 5, 6

--- experiment/.sh
+++ experiment/.sh
@@ -10,7 +10,7 @@
 
 bash venv.sh
 
-# make ./covid-19-news-articles/*-articles_preprocess-01_extract-column-c.csv with LibreOffice
+# LibreOffice: ./covid-19-news-articles/*-articles_preprocess-01_extract-column-c.csv
 
 bash preprocess_02_awk.sh
 # bash preprocess_02_awk.sh -d
@@ -20,8 +20,8 @@ bash preprocess_03_split_sentences.sh -l --spacy
 # bash preprocess_03_split_sentences.sh -ld --stanza
 # bash preprocess_03_split_sentences.sh -ld --spacy
 
-# make ./IBM_Debater_(R)_CE-EMNLP-2015.v3/claims_preprocess-01_extract-column-c with LibreOffice
-# make ./IBM_Debater_(R)_CE-EMNLP-2015.v3/evidence_preprocess-01_extract-column-c with LibreOffice
+# LibreOffice: make ./IBM_Debater_(R)_CE-EMNLP-2015.v3/claims_preprocess-01_extract-column-c
+# LibreOffice: make ./IBM_Debater_(R)_CE-EMNLP-2015.v3/evidence_preprocess-01_extract-column-c
 
 bash preprocess_04_for-debater-dataset.sh
 # bash preprocess_04_for-debater-dataset.sh -d
@@ -29,9 +29,12 @@ bash preprocess_04_for-debater-dataset.sh
 bash preprocess_04_for-debater-dataset.sh
 # bash preprocess_04_for-debater-dataset.sh -d
 
-# bash process_01_train_classifier_as_claim_or_evidence.sh
+bash preprocess_05_split_sentences_for_debater_dataset.sh -l
+# bash preprocess_05_split_sentences_for_debater_dataset.sh -dl
+
+bash preprocess_06_rm_duplicate_sentences.sh -l
+
 bash process_01_train_classifier_as_claim_or_evidence.sh -l
 
-# bash process_02_exe_classifier_as_claim_or_evidence.sh
 bash process_02_exe_classifier_as_claim_or_evidence.sh -l
 # bash process_02_exe_classifier_as_claim_or_evidence.sh -dl
